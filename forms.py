515f36b7455135c81dc74f058e0630cf093ef9b1
zoezhang926 <zoezhang926@gmail.com>
2020-09-12 23:29:36 -0400

    add 404 page, update user edit page etc

--- forms.py
+++ forms.py
@@ -8,6 +8,15 @@ class MessageForm(FlaskForm):
 
     text = TextAreaField('text', validators=[DataRequired()])
 
+class UserEditForm(FlaskForm):
+    """Form for editing users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = TextAreaField('(Optional) Tell us about yourself')
+    password = PasswordField('Password', validators=[Length(min=6)])
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
