162816250e6b0f3860fb48443b8a1d88f35f604d
zoezhang926 <zoezhang926@gmail.com>
2020-09-21 21:57:02 -0400

    add Tests

--- test_message_model.py
+++ test_message_model.py
@@ -1,11 +1,73 @@
-Does the repr method work as expected?
-Does is_following successfully detect when user1 is following user2?
-Does is_following successfully detect when user1 is not following user2?
-Does is_followed_by successfully detect when user1 is followed by user2?
-Does is_followed_by successfully detect when user1 is not followed by user2?
-Does User.create successfully create a new user given valid credentials?
-Does User.create fail to create a new user if any of the validations (e.g. uniqueness, non-nullable fields) fail?
-Does User.authenticate successfully return a user when given a valid username and password?
-Does User.authenticate fail to return a user when the username is invalid?
-Does User.authenticate fail to return a user when the password is invalid?
-Try to formulate a similar set of questions for the Message model.
+import os
+from unittest import TestCase
+from sqlalchemy import exc
+
+from models import db, User, Message, Follows, Likes
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+from app import app
+
+db.create_all()
+
+class UserModelTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
+
+        self.uid = 94566
+        u = User.signup("testing", "testing@test.com", "password", None)
+        u.id = self.uid
+        db.session.commit()
+
+        self.u = User.query.get(self.uid)
+
+        self.client = app.test_client()
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+    def test_message_model(self):
+        """Does basic model work?"""
+        
+        m = Message(
+            text="a warble",
+            user_id=self.uid
+        )
+
+        db.session.add(m)
+        db.session.commit()
+
+        # User should have 1 message
+        self.assertEqual(len(self.u.messages), 1)
+        self.assertEqual(self.u.messages[0].text, "a warble")
+
+    def test_message_likes(self):
+        m1 = Message(
+            text="a warble",
+            user_id=self.uid
+        )
+
+        m2 = Message(
+            text="a very interesting warble",
+            user_id=self.uid 
+        )
+
+        u = User.signup("yetanothertest", "t@email.com", "password", None)
+        uid = 888
+        u.id = uid
+        db.session.add_all([m1, m2, u])
+        db.session.commit()
+
+        u.likes.append(m1)
+
+        db.session.commit()
+
+        l = Likes.query.filter(Likes.user_id == uid).all()
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l[0].message_id, m1.id)
